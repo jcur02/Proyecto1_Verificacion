561586aeab26b378e6bd5a997c0d359c9a0ae96f
jcur02 <jcur1802@gmail.com>
2025-10-21 01:21:41 -0600

    Add Interfaces

--- env.sv
+++ env.sv
@@ -21,3 +21,53 @@ class rx_item;
   constraint c_legal { (tr.rx_size!=4) || (tr.rx_offset==0); }
   function void randomize_data(); tr.data = $urandom(); endfunction
 endclass
+
+// Interfaces
+// Interfaz APB para la comunicacion con los registros
+interface apb_if #(parameter AW=16, DW=32) (input logic clk);
+  logic [AW-1:0] paddr;  
+  logic          pwrite, psel, penable;
+  logic [DW-1:0] pwdata;
+  logic          pready;
+  logic [DW-1:0] prdata;
+  logic          pslverr;
+
+  task automatic write(input logic [AW-1:0] addr, input logic [DW-1:0] data);
+    @(negedge clk);
+    paddr<=addr; pwdata<=data; pwrite<=1; psel<=1; penable<=0;
+    @(negedge clk);
+    penable<=1; while(!pready) @(negedge clk);
+    @(negedge clk);
+    psel<=0; penable<=0; pwrite<=0; paddr<='0; pwdata<='0;
+  endtask
+
+  task automatic read(input logic [AW-1:0] addr, output logic [DW-1:0] data);
+    @(negedge clk);
+    paddr<=addr; pwrite<=0; psel<=1; penable<=0;
+    @(negedge clk);
+    penable<=1; while(!pready) @(negedge clk);
+    data = prdata;
+    @(negedge clk);
+    psel<=0; penable<=0; paddr<='0;
+  endtask
+endinterface
+
+// Interfaz MD para RX
+interface md_rx_if #(parameter DW=32, OW=$clog2(DW/8), SW=$clog2(DW/8)+1) (input logic clk);
+  logic                    valid;
+  logic [DW-1:0]          data;
+  logic [OW-1:0]          offset;
+  logic [SW-1:0]          size;
+  logic                    ready;
+  logic                    err;
+endinterface
+
+// Interfaz MD para TX
+interface md_tx_if #(parameter DW=32, OW=$clog2(DW/8), SW=$clog2(DW/8)+1) (input logic clk);
+  logic                    valid;
+  logic [DW-1:0]          data;
+  logic [OW-1:0]          offset;
+  logic [SW-1:0]          size;
+  logic                    ready;
+  logic                    err;
+endinterface
